1fa0f0c039207a02621bbd79a08517686f87d6ef
ZERO <edgargdcv@gmail.com>
2021-03-22 13:43:18 -0600

    feat: added scripts

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aws-ssm-ec2
 Run commands to ec2 instances from boto3
+
+Scripts allow `git pull` on a repository within ec2 instances using SSM - AWS
+
+* [ssm_instanceids.py](./ssm_instance_ids.py) uses `InstanceIds`
+    * For a a limited number of instances, though you can specify up to 50 IDs.
+
+* [ssm_target.py](./ssm_target.py) uses `Target`
+    * Accepts tag key-value pairs to identify the instances to send commands to, you can a send command to tens, hundreds, or thousands of instances at once.
+
+
+**Note: change commands to do other things**
---
+++ ssm_target.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+import boto3
+
+ec2_resource = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
+ssm_client = boto3.client('ssm')
+
+# Configuration
+repo_path = 'repo_path' # repository path on the instance
+tag_name = 'tag:enviroment' # replace <enviroment> with tag name
+tag_value = 'test' # replace with tag name
+
+
+def instances_find(name, value):
+    """Finds instance id's based on tags.
+    Args:
+        name (str): tag name
+        value (str): tag value
+
+    Returns:
+        list: Returns a list of instances found.
+    """
+
+    list_instances = []
+    # filter based on tags
+    filters =[
+        {
+        'Name': name,
+        'Values': [
+            value,
+            ]
+        },
+    ]
+    instances = ec2_resource.instances.filter(Filters=filters)
+    for instance in instances:
+        list_instances.append(instance.id)
+    return list_instances
+
+
+def check_command_status(instance_id, command_id):
+    """Checks the status of a command on an instance
+    """
+    try:
+        time.sleep(2)
+        result = ssm_client.get_command_invocation(
+            CommandId=command_id,
+            InstanceId=instance_id,
+        )
+        print(f"{instance_id}: {result['Status']}")
+    except ssm_client.exceptions.InvocationDoesNotExist:
+        print(f"{instance_id}: Failed")
+
+
+def run_commands_instance(instance_list):
+    '''
+        Perform actions on a list of ec2 instances
+    '''
+
+    commands = [
+        'echo "hello world" > /home/ec2-user/hello.txt', # demo
+        f'cd {repo_path}',
+        'sudo git pull'
+        # do stuff
+    ]
+
+    response = ssm_client.send_command(
+        Targets=[{
+            "Key": "tag:core_api",
+            "Values": ["test"]
+        }],
+        DocumentName='AWS-RunShellScript',
+        Parameters= {'commands':commands},
+    )
+    command_id = response['Command']['CommandId']
+    for instance_id in instance_list:
+        check_command_status(instance_id, command_id)
+
+
+# Find instances
+ec2_list = instances_find(tag_name, tag_value)
+print(ec2_list)
+
+# Run Command
+run_commands = run_commands_instance(ec2_list)
